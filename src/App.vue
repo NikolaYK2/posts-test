1805769cbbac001672b94e6443ca76222899a0b6
Nikolay Kevlich <izmail.k2@gmail.com>
2025-06-05 20:59:47 +0300

    fix: app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Моё Vue-приложение</h1>
+    <router-view />
+    <Posts />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Posts from '@/feature/posts/ui/Posts.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 20px;
+}
+</style>
