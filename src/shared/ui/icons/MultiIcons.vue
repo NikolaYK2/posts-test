5adc0fc59ab8de38474fb27e05c1531b75a882fe
Nikolay Kevlich <izmail.k2@gmail.com>
2025-06-06 21:40:39 +0300

    feat: add MultiIcons.vue

---
+++ src/shared/ui/icons/MultiIcons.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { type IconsId, iconsType } from './IconsType' // путь к index.ts внутри папки icons
+
+const props = defineProps<{
+  iconId: IconsId
+}>()
+
+const IconComponent = computed(() => {
+  return iconsType[props.iconId] ?? null
+})
+</script>
+
+<template>
+  <component :is="IconComponent" v-if="IconComponent" v-bind="$attrs" />
+</template>
