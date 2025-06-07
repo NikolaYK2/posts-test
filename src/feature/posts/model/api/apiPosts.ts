c325a5da403405996f818d8a25c4b7f5f86f62d5
Nikolay Kevlich <izmail.k2@gmail.com>
2025-06-07 13:38:00 +0300

    feat: add apiPosts

---
+++ src/feature/posts/model/api/apiPosts.ts
@@ -0,0 +1,38 @@
+import { api } from '@/shared/api'
+import type { Article, PostsResponse } from '@/feature/posts/model/types'
+
+const POSTS = '/posts'
+
+type Params = {
+  id?: number
+  limit: number
+  skip: number
+  textQuery?: string
+}
+export const apiPosts = {
+  async getPosts({ skip, limit }: Params) {
+    const res = await api.get<PostsResponse>(POSTS, {
+      params: {
+        limit: limit,
+        skip: skip,
+      },
+    })
+    return res.data
+  },
+
+  async searchPosts(textQuery:string) {
+    const textSearch = textQuery && textQuery.trim()
+
+    const res = await api.get<PostsResponse>(`${POSTS}/search`, {
+      params: {
+        q: textSearch,
+      },
+    })
+    return res.data.posts
+  },
+
+  async getPostsId(id: number) {
+    const res = await api.get<Article>(`${POSTS}/${id}`)
+    return res.data
+  },
+}
