dbc698e5485c03fcc047f532469914fcbeebde13
Nikolay Kevlich <izmail.k2@gmail.com>
2025-06-07 18:28:48 +0300

    feat: add createPost and props

--- src/feature/posts/model/api/apiPosts.ts
+++ src/feature/posts/model/api/apiPosts.ts
@@ -1,8 +1,13 @@
 import { api } from '@/shared/api'
-import type { Article, PostsResponse } from '@/feature/posts/model/types'
+import type { Article, CommentsResponse, PostsResponse } from '@/feature/posts/model/types'
 
 const POSTS = '/posts'
 
+type CreatePostParams={
+  postId: number
+  title: string
+  body: string
+}
 type Params = {
   id?: number
   limit: number
@@ -10,9 +15,14 @@ type Params = {
   textQuery?: string
 }
 export const apiPosts = {
+  async createPost({ postId, title, body }: CreatePostParams) {
+    const res = await api.patch<Article>(`${POSTS}/${postId}`, {body, title})
+    return res.data
+  },
   async getPosts({ skip, limit }: Params) {
     const res = await api.get<PostsResponse>(POSTS, {
       params: {
+        select: 'userId,title,body,reactions',
         limit: limit,
         skip: skip,
       },
@@ -20,7 +30,7 @@ export const apiPosts = {
     return res.data
   },
 
-  async searchPosts(textQuery:string) {
+  async searchPosts(textQuery: string) {
     const textSearch = textQuery && textQuery.trim()
 
     const res = await api.get<PostsResponse>(`${POSTS}/search`, {
@@ -33,6 +43,11 @@ export const apiPosts = {
 
   async getPostsId(id: number) {
     const res = await api.get<Article>(`${POSTS}/${id}`)
+    return res.data
+  },
+
+  async getComment(id: number) {
+    const res = await api.get<CommentsResponse>(`${POSTS}/${id}/comments`)
     return res.data
   },
 }
